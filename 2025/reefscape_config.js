d177d9e89a722113898512b7bf8e53a235ddc3f9
Declan Dunn <58826291+Declan0811@users.noreply.github.com>
2025-03-18 00:50:00 -0400

    Update reefscape_config.js

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2020ilch",
+      "defaultValue": "2025GAGWI",
       "required": "true"
     },
     { "name": "Match Level",
